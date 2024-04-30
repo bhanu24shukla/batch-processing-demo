c787ccd10cfb9db05a24997613a9d839e75bf469
Bhanu Shukla <bhanu24shukla@gmail.com>
2024-04-30 11:53:50 +0530

    First commit

---
+++ src/main/java/com/techie/spring/batch/config/CustomerProcessor.java
@@ -0,0 +1,11 @@
+package com.techie.spring.batch.config;
+
+import com.techie.spring.batch.entity.Customer;
+import org.springframework.batch.item.ItemProcessor;
+
+public class CustomerProcessor implements ItemProcessor<Customer, Customer> {
+    @Override
+    public Customer process(Customer customer) throws Exception {
+        return customer;
+    }
+}
